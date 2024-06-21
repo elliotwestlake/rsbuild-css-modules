ea54aad53efaf8c524b5bb50d60cbe860381a77c
Elliot Westlake <17042376+elliotwestlake@users.noreply.github.com>
2024-06-21 11:29:39 +0100

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { LoadingDog } from "../components/LoadingDog/LoadingDog";
+
+const App = () => {
+  return (
+    <div className="theme">
+      <LoadingDog />
+    </div>
+  );
+};
+
+export default App;
